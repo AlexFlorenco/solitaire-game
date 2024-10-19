cd912d119859d48404b2222f32599c84d1ea18cd
AlexFlorenco <aflorenco13@gmail.com>
2024-10-19 18:06:00 -0300

    feat: Add card flip animation to CardWidget

--- lib/app/components/card_widget.dart
+++ lib/app/components/card_widget.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:solitaire/app/components/card_empty_widget.dart';
 import 'package:solitaire/app/models/card_model.dart';
@@ -10,55 +12,73 @@ class CardWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Material(
       color: Colors.transparent,
-      child: model.isFaceUp
-          ? Container(
-              padding: const EdgeInsets.all(4),
-              width: 50,
-              height: 78,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10),
-                border: Border.all(color: Colors.black, width: 0.3),
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        model.value.label,
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                          color: model.suit.color,
-                          decoration: TextDecoration.none,
+      child: AnimatedSwitcher(
+        duration: const Duration(milliseconds: 300),
+        transitionBuilder: (Widget child, Animation<double> animation) {
+          final rotate = Tween(begin: 1.0, end: 0.0).animate(animation);
+
+          return AnimatedBuilder(
+            animation: rotate,
+            child: child,
+            builder: (context, child) {
+              return Transform(
+                transform: Matrix4.rotationY(min(rotate.value, 0.5) * pi),
+                alignment: Alignment.center,
+                child: child,
+              );
+            },
+          );
+        },
+        child: model.isFaceUp
+            ? Container(
+                padding: const EdgeInsets.all(4),
+                width: 50,
+                height: 78,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(color: Colors.black, width: 0.3),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          model.value.label,
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: model.suit.color,
+                            decoration: TextDecoration.none,
+                          ),
                         ),
-                      ),
-                      Text(
-                        model.suit.char,
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                          color: model.suit.color,
-                          decoration: TextDecoration.none,
+                        Text(
+                          model.suit.char,
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: model.suit.color,
+                            decoration: TextDecoration.none,
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                  Text(
-                    model.suit.char,
-                    style: TextStyle(
-                      fontSize: 32,
-                      fontWeight: FontWeight.bold,
-                      color: model.suit.color,
-                      decoration: TextDecoration.none,
+                      ],
                     ),
-                  ),
-                ],
-              ),
-            )
-          : const CardEmptyWidget.back(),
+                    Text(
+                      model.suit.char,
+                      style: TextStyle(
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold,
+                        color: model.suit.color,
+                        decoration: TextDecoration.none,
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            : const CardEmptyWidget.back(),
+      ),
     );
   }
 }
